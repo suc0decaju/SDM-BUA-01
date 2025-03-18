4139bb7dd4e9a7ecf2c0f354735c636fbbf12cfb
Danilo-Miguel <prof.danilomiguel@ulife.com.br>
2025-03-18 11:59:12 -0300

    TRabalhando com monolito

---
+++ monolito.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+let usuarios = []
+let pedidos = []
+
+// Rota para cadastrar usuários
+
+app.post("/usuarios", (req, res) => {
+    const usuario = req.body;
+    usuarios.push(usuario)
+    res.send({message: "Usuario cadastrado com sucesso", usuario})
+})
+
+app.get("/dados", (req, res) =>{
+    res.send({usuarios})
+})
+
+app.listen(3000, () => console.log("Servidor monolitico ok, na porta 3000"))
