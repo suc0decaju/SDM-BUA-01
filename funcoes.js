eb7924a561dd3f3f7b7553ca73495d8a49e542c4
Danilo-Miguel <prof.danilomiguel@ulife.com.br>
2025-03-18 11:03:25 -0300

    Trabalhando com funções

---
+++ funcoes.js
@@ -0,0 +1,16 @@
+function ola(nome){
+    return `Olá, ${nome}`
+}
+
+console.log(ola("Danilo"))
+
+
+const soma =  function(a, b){
+
+return a + b
+}
+console.log(soma(10 , 25)) 
+
+const mult = (x , y) => x * y;
+
+console.log(mult(10 , 25)) 
